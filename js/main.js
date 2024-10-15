c526cf03687265cd0ee79fcf4228dc22c659f0fc
Walter Adrián Olivera <adrian.olivera.11@gmail.com>
2024-10-15 11:24:00 -0300

    correción de reentrega por usar LiveServer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-let empleados = [];
+let empleados = JSON.parse(localStorage.getItem('empleados')) || [];
 
 let sectores = ['Administración', 'Ventas', 'Marketing', 'Sistemas', 'RRHH', 'Legal', 'Compras', 'Producción'];
 
-// CONTROL DE VISION DE BOTONES
+// CONTROL DE VISIÓN DE BOTONES
 function viewFuntionEmployee(divId) {
     // Ocultar todos los contenedores
     document.getElementById('containerListEmployees').style.display = 'none';
@@ -10,36 +10,53 @@ function viewFuntionEmployee(divId) {
     document.getElementById('containerAddEmployees').style.display = 'none';
     document.getElementById('containerDeleteEmployees').style.display = 'none';
 
-    document.getElementById(divId).style.display = 'block';
+    // Limpiar mensajes de resultados anteriores
+    document.getElementById('resultAddEmployee').innerHTML = '';
+    document.getElementById('resultDeleteEmployee').innerHTML = '';
+    document.getElementById('resultSearchEmployee').innerHTML = '';
+
+    // Si divId no es null o undefined, mostrar el contenedor correspondiente
+    if (divId) {
+        document.getElementById(divId).style.display = 'block';
+    }
 }
 
 document.getElementById('btnListEmployees').addEventListener('click', function() {
-    viewFuntionEmployee('containerListEmployees'); // Mostrar el contenedor de "Listar Empleados"
-    listEmployees(empleados);
+    viewFuntionEmployee('containerListEmployees');
+    listEmployees();
 });
 
 document.getElementById('btnSearchEmployees').addEventListener('click', function() {
-    viewFuntionEmployee('containerSearchEmployees'); // Mostrar el contenedor de "Buscar Empleados"
+    viewFuntionEmployee('containerSearchEmployees');
     document.getElementById('resultSearchEmployee').innerHTML = '';
-    SearchEmployees();
 });
 
 document.getElementById('btnAddEmployees').addEventListener('click', function() {
-    viewFuntionEmployee('containerAddEmployees'); // Mostrar el contenedor de "Agregar Empleados"
-    addEmployees();
+    viewFuntionEmployee('containerAddEmployees');
+    listSectors(sectores);
+    controlChildren();
 });
 
 document.getElementById('btnDeleteEmployees').addEventListener('click', function() {
-    viewFuntionEmployee('containerDeleteEmployees'); // Mostrar el contenedor de "Eliminar Empleados"
+    viewFuntionEmployee('containerDeleteEmployees');
+});
+
+document.getElementById('btnSearchEmployee').addEventListener('click', function() {
+    SearchEmployees();
+});
+
+document.getElementById('btnDeleteEmployee').addEventListener('click', function() {
     deleteEmployee();
 });
 
-viewFuntionEmployee(null);
+document.getElementById('saveEmployee').addEventListener('click', function() {
+    addEmployees();
+});
 
-// FIN DE CONTROL DE VISION DE BOTON
+viewFuntionEmployee();
 
 // LISTAR EMPLEADOS
-function listEmployees(empleados){
+function listEmployees() {
     const tbody = document.getElementById('dataListEmployees');
     tbody.innerHTML = '';
     const storedEmpleados = localStorage.getItem('empleados');
@@ -51,7 +68,7 @@ function listEmployees(empleados){
             row.innerHTML = `
                 <th scope="row">${empleado.legajo}</th>
                 <td>${empleado.empleado}</td>
-                <td>${empleado.sector}</td>
+                <td>${empleado.sector}</th>
                 <td>${empleado.hijos}</td>
             `;
             tbody.appendChild(row);
@@ -60,43 +77,38 @@ function listEmployees(empleados){
 }
 
 // BUSCAR EMPLEADO
-function SearchEmployees () {
-    const btnSearchEmployees = document.getElementById('btnSearchEmployee');
+function SearchEmployees() {
+    const inputLegajo = document.getElementById('inputSearchEmployee').value;
     const localStorageEmployees = JSON.parse(localStorage.getItem('empleados'));
-
-    btnSearchEmployees.onclick = () => {
-        
-        const inputLegajo = document.getElementById('inputSearchEmployee').value;
-        const searchEmployee = localStorageEmployees.find(employee => employee.legajo === inputLegajo);
-        const resultSearchEmployee = document.getElementById('resultSearchEmployee');
-        
-        if (searchEmployee) {
-            resultSearchEmployee.innerHTML = `
-                            <table class="table">
-                                <thead class="table-primary">
-                                <tr>
-                                    <th scope="col">Legajo</th>
-                                    <th scope="col">Empleado</th>
-                                    <th scope="col">Sector</th>
-                                    <th scope="col">Cantidad de Hijos/as</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">${searchEmployee.legajo}</th>
-                                        <td>${searchEmployee.empleado}</td>
-                                        <td>${searchEmployee.sector}</td>
-                                        <td>${searchEmployee.hijos}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        `
-        } else {
-            resultSearchEmployee.innerHTML = `<h6 class="marginElement mensergerDanger">No se encontró el empleado con legajo ${inputLegajo}</h6>`;
-        }
-
-        document.getElementById('inputSearchEmployee').value = '';
+    const searchEmployee = localStorageEmployees.find(employee => employee.legajo === inputLegajo);
+    const resultSearchEmployee = document.getElementById('resultSearchEmployee');
+    
+    if (searchEmployee) {
+        resultSearchEmployee.innerHTML = `
+            <table class="table">
+                <thead class="table-primary">
+                <tr>
+                    <th scope="col">Legajo</th>
+                    <th scope="col">Empleado</th>
+                    <th scope="col">Sector</th>
+                    <th scope="col">Cantidad de Hijos/as</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">${searchEmployee.legajo}</th>
+                        <td>${searchEmployee.empleado}</td>
+                        <td>${searchEmployee.sector}</td>
+                        <td>${searchEmployee.hijos}</td>
+                    </tr>
+                </tbody>
+            </table>
+        `;
+    } else {
+        resultSearchEmployee.innerHTML = `<h6 class="marginElement mensergerDanger">No se encontró el empleado con legajo ${inputLegajo}</h6>`;
     }
+
+    document.getElementById('inputSearchEmployee').value = '';
 }
 
 function controlChildren() {
@@ -134,82 +146,59 @@ function listSectors(sectores) {
     }
 }
 
-//AGREGAR EMPLEADO
+// AGREGAR EMPLEADO
 function addEmployees() {
-
-    listSectors(sectores);
-    controlChildren();
-
-    const buttonAddEmployee = document.getElementById('saveEmployee');
+    const legajo = document.getElementById('legajo').value;
+    const empleado = document.getElementById('empleado').value;
+    const sector = document.getElementById('selectSector').value;
+    const hijos = document.getElementById('countChildren').textContent;
     const resultAddEmployee = document.getElementById('resultAddEmployee');
-    resultAddEmployee.innerHTML = '';
 
-    buttonAddEmployee.onclick = () => {
-        const legajo = document.getElementById('legajo').value;
-        const empleado = document.getElementById('empleado').value;
-        const sector = document.getElementById('selectSector').value;
-        const hijos = document.getElementById('countChildren').textContent;
+    const localStorageEmployees = JSON.parse(localStorage.getItem('empleados'));
 
-        const localStorageEmployees = JSON.parse(localStorage.getItem('empleados'));
+    // Validar si ya existe el legajo
+    const searchEmployee = localStorageEmployees.find(employee => employee.legajo === legajo);
 
-        //VALIDA PRIMERO SI EL LOCALSTORAGE ESTA VACIO PARA PERMITIR EL PRIMER REGISTRO Y PARA LUEGO VALIDAR SI YA EXISTE EL LEGAJO
-        if (localStorageEmployees !== null) {
-            const searchEmployee = localStorageEmployees.find(employee => employee.legajo === legajo);
-
-            if (searchEmployee) {
-                resultAddEmployee.innerHTML = `<h6 class="marginElement mensergerDanger">Ya existe un empleado con legajo ${legajo}</h6>`;
-                return;
-            } else {
-                const newEmployee = {
-                    id: empleados.length, // Asignar un ID basado en la longitud del array
-                    legajo: legajo,
-                    empleado: empleado,
-                    sector: sector,
-                    hijos: hijos
-                };
-            
-                empleados.push(newEmployee);
-        
-                localStorage.setItem('empleados', JSON.stringify(empleados));
-        
-                document.getElementById('legajo').value = '';
-                document.getElementById('empleado').value = '';
-                document.getElementById('selectSector').value = '';
-                document.getElementById('countChildren').textContent = 0;
-        
-                resultAddEmployee.innerHTML = `<h6 class="marginElement mensergerSuccess">Empleado con legajo ${legajo} agregado correctamente</h6>`;
-            }
-        }
-        
-     
-        
-        
-
-        
+    if (searchEmployee) {
+        resultAddEmployee.innerHTML = `<h6 class="marginElement mensergerDanger">Ya existe un empleado con legajo ${legajo}</h6>`;
+        return;
+    } else {
+        const newEmployee = {
+            id: localStorageEmployees.length, // Asignar un ID basado en la longitud del array
+            legajo: legajo,
+            empleado: empleado,
+            sector: sector,
+            hijos: hijos
+        };
+    
+        localStorageEmployees.push(newEmployee);
+        localStorage.setItem('empleados', JSON.stringify(localStorageEmployees));
+    
+        document.getElementById('legajo').value = '';
+        document.getElementById('empleado').value = '';
+        document.getElementById('selectSector').value = '';
+        document.getElementById('countChildren').textContent = 0;
+    
+        resultAddEmployee.innerHTML = `<h6 class="marginElement mensergerSuccess">Empleado con legajo ${legajo} agregado correctamente</h6>`;
     }
 }
 
 // ELIMINAR EMPLEADO
-
 function deleteEmployee() {
-    const btnDeleteEmployee = document.getElementById('btnDeleteEmployee');
+    const inputLegajo = document.getElementById('inputDeleteEmployee').value;
     const localStorageEmployees = JSON.parse(localStorage.getItem('empleados'));
     const resultDeleteEmployee = document.getElementById('resultDeleteEmployee');
-    resultDeleteEmployee.innerHTML = '';
 
-    btnDeleteEmployee.onclick = () => {
-        const inputLegajo = document.getElementById('inputDeleteEmployee').value;
-        const searchEmployee = localStorageEmployees.find(employee => employee.legajo === inputLegajo);
+    const searchEmployee = localStorageEmployees.find(employee => employee.legajo === inputLegajo);
 
-        if (searchEmployee) {
-            const index = localStorageEmployees.indexOf(searchEmployee);
-            localStorageEmployees.splice(index, 1);
-            localStorage.setItem('empleados', JSON.stringify(localStorageEmployees));
-            resultDeleteEmployee.innerHTML = `<h6 class="marginElement mensergerSuccess">Empleado con legajo ${inputLegajo} eliminado correctamente</h6>`;
-        } else {
-            resultDeleteEmployee.innerHTML = `<h6 class="marginElement mensergerDanger">No se encontró el empleado con legajo ${inputLegajo}</h6>`;
-        }
-
-        document.getElementById('inputDeleteEmployee').value = '';
+    if (searchEmployee) {
+        const index = localStorageEmployees.indexOf(searchEmployee);
+        localStorageEmployees.splice(index, 1);
+        localStorage.setItem('empleados', JSON.stringify(localStorageEmployees));
+        resultDeleteEmployee.innerHTML = `<h6 class="marginElement mensergerSuccess">Empleado con legajo ${inputLegajo} eliminado correctamente</h6>`;
+    } else {
+        resultDeleteEmployee.innerHTML = `<h6 class="marginElement mensergerDanger">No se encontró el empleado con legajo ${inputLegajo}</h6>`;
     }
+
+    document.getElementById('inputDeleteEmployee').value = '';
 }
