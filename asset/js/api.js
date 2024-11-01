f9785e9ff193fd8cea26645365dd42e38f74b252
Walter Adrián Olivera <adrian.olivera.11@gmail.com>
2024-11-01 09:58:14 -0300

    implento list and search pokemon

---
+++ asset/js/api.js
@@ -0,0 +1,102 @@
+const pokemonsList = fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151')
+    .then(response => response.json())
+    .then(data => new Promise((resolve, reject) => {
+        resolve(data.results);
+        return data.results;
+    }));
+
+function selectOnePokemon(url) {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => data);
+}
+
+// CONTROL DE VISIÓN DE BOTONES
+function viewFuntionPokemon(divId) {
+    // Ocultar todos los contenedores
+    document.getElementById('containerListPokemons').style.display = 'none';
+    document.getElementById('containerSearchPokemons').style.display = 'none';
+
+    // Limpiar mensajes de resultados anteriores
+    document.getElementById('resultSearchPokemon').innerHTML = '';
+
+    // Si divId no es null o undefined, mostrar el contenedor correspondiente
+    if (divId) {
+        document.getElementById(divId).style.display = 'block';
+    }
+}
+
+document.getElementById('btnListPokemons').addEventListener('click', function() {
+    viewFuntionPokemon('containerListPokemons');
+    listPokemons();
+});
+
+document.getElementById('btnSearchPokemons').addEventListener('click', function() {
+    viewFuntionPokemon('containerSearchPokemons');
+    document.getElementById('resultSearchPokemon').innerHTML = '';
+});
+
+document.getElementById('btnSearchPokemon').addEventListener('click', function() {
+    SearchPokemons();
+});
+
+viewFuntionPokemon();
+
+// LISTAR POKEMONS
+function listPokemons() {
+    const container = document.getElementById('pokemonCardContainer');
+    container.innerHTML = '';
+    
+    pokemonsList.then(data => {
+        data.forEach(pokemon => {
+            const col = document.createElement('div');
+            col.classList.add('col-md-2');
+            
+            selectOnePokemon(pokemon.url).then(select => {
+                col.innerHTML = `
+                    <div class="card">
+                        <img src="${select.sprites.other["official-artwork"].front_default}" class="card-img-top" alt="${pokemon.name}">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">${pokemon.name}</h5>
+                            <p>${select.id}</p>
+                        </div>
+                    </div>
+                `;
+
+                container.appendChild(col);
+            });
+        });
+    });
+}
+
+function SearchPokemons() {
+    const inputSearch = document.getElementById('inputSearchPokemon').value;
+    const container = document.getElementById('resultSearchPokemon');
+    container.innerHTML = '';
+    
+    const col = document.createElement('div');
+    col.classList.add('col-md-2');
+    
+    if (inputSearch > 151) {
+        container.innerHTML = `<h6 class="marginElement mensergerDanger">No se encontró el pokemon con id ${inputSearch}</h6>`;
+    } else {
+        selectOnePokemon(`https://pokeapi.co/api/v2/pokemon/${inputSearch}`).then(select => {
+            if (select) {
+                col.innerHTML = `
+                    <div class="card">
+                        <img src="${select.sprites.other["official-artwork"].front_default}" class="card-img-top" alt="${select.name}">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">${select.name}</h5>
+                            <p>ID: ${select.id}</p>
+                        </div>
+                    </div>
+                `;
+                container.appendChild(col);
+            }
+        }).catch(error => {
+            container.innerHTML = `<p class="text-danger">Error al buscar el Pokémon: ${error.message}</p>`;
+        });
+    
+        document.getElementById('inputSearchPokemon').value = '';    
+    }
+};
---
+++ asset/js/main.js
@@ -0,0 +1,198 @@
+let entrenadores = JSON.parse(localStorage.getItem('entrenadores')) || [];
+
+let sectores = ['Administración', 'Ventas', 'Marketing', 'Sistemas', 'RRHH', 'Legal', 'Compras', 'Producción'];
+
+// CONTROL DE VISIÓN DE BOTONES
+function viewFuntionCoach(divId) {
+    // Ocultar todos los contenedores
+    document.getElementById('containerListCoachs').style.display = 'none';
+    document.getElementById('containerSearchCoachs').style.display = 'none';
+    document.getElementById('containerAddCoachs').style.display = 'none';
+    document.getElementById('containerDeleteCoachs').style.display = 'none';
+
+    // Limpiar mensajes de resultados anteriores
+    document.getElementById('resultAddCoach').innerHTML = '';
+    document.getElementById('resultDeleteCoach').innerHTML = '';
+    document.getElementById('resultSearchCoach').innerHTML = '';
+
+    // Si divId no es null o undefined, mostrar el contenedor correspondiente
+    if (divId) {
+        document.getElementById(divId).style.display = 'block';
+    }
+}
+
+document.getElementById('btnListCoachs').addEventListener('click', function() {
+    viewFuntionCoach('containerListCoachs');
+    listCoachs();
+});
+
+document.getElementById('btnSearchCoachs').addEventListener('click', function() {
+    viewFuntionCoach('containerSearchCoachs');
+    document.getElementById('resultSearchCoach').innerHTML = '';
+});
+
+document.getElementById('btnAddCoachs').addEventListener('click', function() {
+    viewFuntionCoach('containerAddCoachs');
+    listSectors(sectores);
+    controlChildren();
+});
+
+document.getElementById('btnDeleteCoachs').addEventListener('click', function() {
+    viewFuntionCoach('containerDeleteCoachs');
+});
+
+document.getElementById('btnSearchCoach').addEventListener('click', function() {
+    SearchCoachs();
+});
+
+document.getElementById('btnDeleteCoach').addEventListener('click', function() {
+    deleteCoach();
+});
+
+document.getElementById('saveCoach').addEventListener('click', function() {
+    addCoachs();
+});
+
+viewFuntionCoach();
+
+// LISTAR ENTRENADORES
+function listCoachs() {
+    const tbody = document.getElementById('dataListCoachs');
+    tbody.innerHTML = '';
+    
+    if (entrenadores) {
+        entrenadores.forEach(entrenador => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <th scope="row">${entrenador.id}</th>
+                <td>${entrenador.entrenador}</td>
+                <td>${entrenador.sector}</th>
+                <td>${entrenador.hijos}</td>
+            `;
+            tbody.appendChild(row);
+        });
+    }
+}
+
+// BUSCAR ENTRENADOR
+function SearchCoachs() {
+    const inputId = document.getElementById('inputSearchCoach').value;
+    const searchCoach = entrenadores.find(Coach => Coach.id === inputId);
+    const resultSearchCoach = document.getElementById('resultSearchCoach');
+    
+    if (searchCoach) {
+        resultSearchCoach.innerHTML = `
+            <table class="table">
+                <thead class="table-primary">
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Entrenador</th>
+                    <th scope="col">Sector</th>
+                    <th scope="col">Cantidad de Hijos/as</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">${searchCoach.id}</th>
+                        <td>${searchCoach.entrenador}</td>
+                        <td>${searchCoach.sector}</td>
+                        <td>${searchCoach.hijos}</td>
+                    </tr>
+                </tbody>
+            </table>
+        `;
+    } else {
+        resultSearchCoach.innerHTML = `<h6 class="marginElement mensergerDanger">No se encontró el entrenador con id ${inputId}</h6>`;
+    }
+
+    document.getElementById('inputSearchCoach').value = '';
+}
+
+function controlChildren() {
+    let count = document.getElementById('countChildren');
+    let subtract = document.getElementById('subtractChildren');
+    let add = document.getElementById('addChildren');
+    let counter = 0;
+
+    subtract.onclick = () => {
+        if (counter > 0) {
+            counter--;
+            count.innerHTML = counter;
+        }
+    }
+    add.onclick = () => {
+        counter++;
+        count.innerHTML = counter;
+    }
+}
+
+// LISTAR SECTORES
+function listSectors(sectores) {
+    const selectSector = document.getElementById('selectSector');
+    if (selectSector) {
+        // Limpiar cualquier opción existente
+        selectSector.innerHTML = '';
+
+        // Agregar nuevas opciones
+        for (const sector of sectores) {
+            const option = document.createElement('option');
+            option.value = sector;
+            option.textContent = sector;
+            selectSector.appendChild(option);
+        }
+    }
+}
+
+// AGREGAR ENTRENADOR
+function addCoachs() {
+    const id = document.getElementById('id').value;
+    const entrenador = document.getElementById('entrenador').value;
+    const sector = document.getElementById('selectSector').value;
+    const hijos = document.getElementById('countChildren').textContent;
+    const resultAddCoach = document.getElementById('resultAddCoach');
+
+    // Validar si ya existe el id
+    const searchCoach = entrenadores.find(Coach => Coach.id === id);
+
+    if (searchCoach) {
+        resultAddCoach.innerHTML = `<h6 class="marginElement mensergerDanger">Ya existe un entrenador con id ${id}</h6>`;
+        return;
+    } else {
+        const newCoach = {
+            id: entrenadores.length, // Asignar un ID basado en la longitud del array
+            id: id,
+            entrenador: entrenador,
+            sector: sector,
+            hijos: hijos
+        };
+    
+        entrenadores.push(newCoach);
+        localStorage.setItem('entrenadores', JSON.stringify(entrenadores));
+    
+        document.getElementById('id').value = '';
+        document.getElementById('entrenador').value = '';
+        document.getElementById('selectSector').value = '';
+        document.getElementById('countChildren').textContent = 0;
+    
+        resultAddCoach.innerHTML = `<h6 class="marginElement mensergerSuccess">Entrenador con id ${id} agregado correctamente</h6>`;
+    }
+}
+
+// ELIMINAR ENTRENADOR
+function deleteCoach() {
+    const inputId = document.getElementById('inputDeleteCoach').value;
+    const resultDeleteCoach = document.getElementById('resultDeleteCoach');
+
+    const searchCoach = entrenadores.find(Coach => Coach.id === inputId);
+
+    if (searchCoach) {
+        const index = entrenadores.indexOf(searchCoach);
+        entrenadores.splice(index, 1);
+        localStorage.setItem('entrenadores', JSON.stringify(entrenadores));
+        resultDeleteCoach.innerHTML = `<h6 class="marginElement mensergerSuccess">Entrenador con id ${inputId} eliminado correctamente</h6>`;
+    } else {
+        resultDeleteCoach.innerHTML = `<h6 class="marginElement mensergerDanger">No se encontró el entrenador con id ${inputId}</h6>`;
+    }
+
+    document.getElementById('inputDeleteCoach').value = '';
+}
